073624374e369c2fb757788f4f0c00c9e5a60232
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-10-13 22:58:37 -0500

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Justin Hold on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
